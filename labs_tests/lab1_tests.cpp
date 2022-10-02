ea17a20a7ccb36a1965239cdc15ef983efffbcf8
IvanNazarkin <70940509+IvanNazarkin@users.noreply.github.com>
2022-10-02 22:07:56 +0400

    Initial commit

---
+++ labs_tests/lab1_tests.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "lab1/lab1_functions.h"
+
+TEST(CreatePolygonalChain, PerimeterCalculatedCorrectly) {
+    
+}
+
+TEST(CreateClosedPlyChain, PerimeterAndAreaCalculatedCorrectly) {
+    
+}
+
+TEST(CreateSharpTriangle, RightTriangleCheckFailed_ThrowException) {
+    
+}
+
+TEST(CreateTrapezoid, PerimeterAndAreaCalculatedCorrectly){
+
+}
+
+
